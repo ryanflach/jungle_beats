a8d09bf9249588305d21c3a571a736aaa681fb5f
Ryan Flach <ryanflach@users.noreply.github.com>
2016-05-17 19:47:42 -0600

    Added tests and methods for extension 2

--- lib/jungle_beat.rb
+++ lib/jungle_beat.rb
@@ -2,9 +2,12 @@ require './lib/linked_list'
 
 class JungleBeat
   attr_reader :list
+  attr_accessor :rate, :voice
 
   def initialize
     @list = LinkedList.new
+    @rate = 500
+    @voice = "Boing"
   end
 
   def append(data)
@@ -12,12 +15,25 @@ class JungleBeat
     data_list.each { |data| list.append(data) }
   end
 
+  def prepend(data)
+    data_list = data.split.reverse
+    data_list.each { |data| list.prepend(data) }
+  end
+
   def count
     list.count
   end
 
   def play
-    `say -r 500 -v Boing #{list.to_string}`
+    `say -r #{rate} -v #{voice} #{list.to_string}`
+  end
+
+  def reset_rate
+    @rate = 500
+  end
+
+  def reset_voice
+    @voice = "Boing"
   end
 
 end
--- test/jungle_beat_test.rb
+++ test/jungle_beat_test.rb
@@ -23,6 +23,18 @@ class JungleBeatTest < Minitest::Test
     assert_equal "diggy", @jb.list.tail.data
   end
 
+  def test_it_can_prepend_to_a_linked_list
+    @jb.prepend("doo")
+    assert_equal "doo", @jb.list.head.data
+  end
+
+  def test_it_can_prepend_multiple_items_at_once
+    @jb.append("ray")
+    @jb.prepend("hip hip hip hop hoo")
+    assert_equal "hip", @jb.list.head.data
+    assert_equal "hoo", @jb.list.navigate_to(4).data
+  end
+
   def test_it_can_count_the_number_of_items
     @jb.append("doo wop diggy")
     assert_equal 3, @jb.count
@@ -33,5 +45,26 @@ class JungleBeatTest < Minitest::Test
     assert @jb.play
   end
 
+  def test_speed_can_be_changed
+    @jb.rate = 100
+    assert_equal 100, @jb.rate
+  end
+
+  def test_speed_can_be_reset
+    @jb.rate = 100
+    @jb.reset_rate
+    assert_equal 500, @jb.rate
+  end
+
+  def test_voice_can_be_changed
+    @jb.voice = "Alice"
+    assert_equal "Alice", @jb.voice
+  end
+
+  def test_voice_can_be_reset
+    @jb.voice = "Alice"
+    @jb.reset_voice
+    assert_equal "Boing", @jb.voice
+  end
 
 end
